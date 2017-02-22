7bde18ffced34cbdefd83ca5c1966ef03f020c25
Sheehan <ryan.sheehan@tylertech.com>
2017-02-22 13:02:54 -0600

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import Vue = require('vue');
+import App from './App.vue';
+import router from './router';
+
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }  
+});
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import Vue = require('vue');
+import * as Router from 'vue-router';
+import Hello from 'components/Hello.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    }
+  ]
+});
