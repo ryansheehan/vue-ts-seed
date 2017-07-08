33167d300016f431c38f202e5f1ccb0af8d28eb9
Ryan Sheehan <ryansheehan@users.noreply.github.com>
2017-07-08 14:45:45 -0500

    remove old config settings

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -3,12 +3,12 @@ import * as firebase from 'firebase/app';
 
 // App initialization
 const firebaseApp: firebase.app.App = firebase.initializeApp({
-  apiKey: "AIzaSyAo6xJ0zsKWkrIzPOET0KfEiFrVD7SkD24",
-  authDomain: "picjade-11cec.firebaseapp.com",
-  databaseURL: "https://picjade-11cec.firebaseio.com",
+  apiKey: "<api_key>",
+  authDomain: "<auth>.firebaseapp.com",
+  databaseURL: "https://<db>.firebaseio.com",
   storageBucket: "",
-  messagingSenderId: "93538683713"
-}, "emotivent");
+  messagingSenderId: "<msg_id>"
+}, "vue-ts-seed");
 
 
 export default firebaseApp;
